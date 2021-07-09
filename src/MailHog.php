ae0291afa9531647ee1fe0994ee146c72f978775
Sven Liebert <sven.liebert@telekom.de>
2021-07-09 09:53:08 +0200

    Debug

--- src/MailHog.php
+++ src/MailHog.php
@@ -519,6 +519,7 @@ class MailHog extends Module
    */
   protected function sortEmails($inbox)
   {
+      var_dump($inbox);
     usort($inbox, array($this, 'sortEmailsByCreationDatePredicate'));
   }
 
