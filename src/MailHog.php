b39cf4288ecc5556f468b0a5cd77e947b2ee497e
Sven Liebert <sven.liebert@telekom.de>
2021-08-26 12:40:56 +0200

    UTF8 decode

--- src/MailHog.php
+++ src/MailHog.php
@@ -445,7 +445,7 @@ class MailHog extends Module
       ) {
           $property = quoted_printable_decode($property);
       }
-      if (strpos($property, '=?utf-8?Q?') !== false && extension_loaded('mbstring')) {
+      if ((strpos($property, '=?utf-8?Q?') !== false || strpos($property,"'=?UTF-8?q?")) && extension_loaded('mbstring')) {
         $property = mb_decode_mimeheader($property);
       }
     }
